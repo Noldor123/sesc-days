df0af13c8d812b522124134175151f6fb31f398b
Noldor123 <37693340+Noldor123@users.noreply.github.com>
2018-10-05 22:36:17 +0700

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,10 @@
+int main(){
+  int days, weeks, years;
+  scanf("%d", &days);
+  years = floor(days/365);
+  days = days%365;
+  weeks= floor(days/7)
+  days=days%7;
+  printf("years: %d\nweeks: %d\ndays: %d", years, weeks, days);
+  return 0;
+}
